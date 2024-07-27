36f5c0298d2927431fe983646c28e63e6d85c7f3
Sahilbalhra <sahilbalhra0@gmail.com>
2024-07-27 12:59:36 +0530

    .

---
+++ src/model/user.model.ts
@@ -0,0 +1,69 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IMessage extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+const MessageSchema: Schema<IMessage> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
+  isAcceptingMessage: boolean;
+  message: IMessage[];
+}
+
+const UserSchema: Schema<IUser> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/.+\@.+\..+/, "Please use a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "Verify Code is required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    default: false,
+  },
+  message: [MessageSchema],
+});
+
+const UserModel =
+  (mongoose.models.User as mongoose.Model<IUser>) ||
+  mongoose.model<IUser>("User", UserSchema);
+
+export default UserModel;
